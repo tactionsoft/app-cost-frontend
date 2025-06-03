bbb70838db206caac289613443bafe7916a445d9
Alok <aloksingh@192.168.1.2>
2025-06-03 17:30:50 +0530

    alok-ec: UI style Updates 3june25-1.

--- src/components/PageFifteen/PageFifteen.js
+++ src/components/PageFifteen/PageFifteen.js
@@ -359,7 +359,7 @@ const handleSubmit = async (e) => {
 
   return (
 
-    <main className="pt5 black-80 center-fifteen form-content fifteen-content">
+    <main className="pt5 black-80 center-fifteen form-content fifteen-content container">
         <div className="total-est-cost well">
         <h2 className="total-cost-title">Estimated Cost based on your total selection: <span id="total-selection-cost"><br/>{totalCost}</span>
         </h2>
